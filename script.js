544e260d3153acc9bcdbce9d16e6ebf0c8aeafdf
gabrielgebert <gabrielgebert@yahoo.com.br>
2025-01-14 13:29:03 +0000

    second commit (clickable entity show text)

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("DOMContentLoaded");
     const sceneEl = document.querySelector('a-scene');
     const switchButton = document.getElementById("next-button");
-    var showText = true;
+    const sunModel = document.getElementById("sun-model");
+    var showTextSun = false;
     var text = document.getElementById("textAFrame");
     
     switchButton.addEventListener("click", () => {
@@ -10,5 +11,12 @@ document.addEventListener("DOMContentLoaded", function () {
         text.setAttribute("visible", showText);
         text.setAttribute ("value", "Funcionou!");
         console.log("teste");
+    });
+
+    sunModel.addEventListener("click", () =>{
+        console.log("click!")
+        const sunPanel = document.getElementById("sun-panel");
+        showTextSun = !showTextSun;
+        sunPanel.setAttribute("visible", showTextSun);
     });
 });
